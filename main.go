cff23f508f33d5bf512a7093395dc98fedb7c3d7
Tiger Abrodi <tigerabrodi@gmail.com>
2025-05-26 22:07:49 +0200

    main.go cleanup and make it moreproper

--- main.go
+++ main.go
@@ -23,10 +23,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	linesCount := countLines(data)
-	wordsCount := countWords(data)
+	stats := analyzeText(data)
 
-	content := fmt.Sprintf("Words: %d\nlines: %d\n", wordsCount, linesCount)
+	content := fmt.Sprintf("Here are all your stats\n"+
+		"Word count %d\n"+
+		"Lines count: %d"+
+		"Char count: %d"+
+		"Avg word len %f",
+		stats.wordsCount, stats.linesCount, stats.charsCount, stats.avgWordLen)
 
 	os.WriteFile(statsFilename, []byte(content), 0644)
 
@@ -37,5 +41,28 @@ func countLines(data []byte) int {
 }
 
 func countWords(data []byte) int {
-	return bytes.Count(data, []byte{' '}) + 1
+	wordSlices := bytes.Fields(data)
+	return len(wordSlices)
+}
+
+func countChars(data []byte) int {
+	bytesWithoutWeirdWindowsBullshit := bytes.ReplaceAll(data, []byte{'\r'}, []byte{})
+	return len(bytesWithoutWeirdWindowsBullshit)
+}
+
+type textStats struct {
+	wordsCount int
+	charsCount int
+	linesCount int
+	avgWordLen float64
+}
+
+func analyzeText(data []byte) textStats {
+	stats := textStats{}
+
+	stats.linesCount = countLines(data)
+	stats.wordsCount = countWords(data)
+	stats.charsCount = countChars(data)
+
+	return stats
 }
